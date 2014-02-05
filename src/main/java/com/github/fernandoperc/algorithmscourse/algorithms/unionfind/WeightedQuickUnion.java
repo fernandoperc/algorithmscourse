ce8e9dc3b0758ec95930da9655774fae87fda0a7
Fernando Ferraz <fernandoperches@gmail.com>
2014-02-05 00:51:04 -0200

    adding WeigthedQuickUnion class (not implemented yet)

---
+++ src/main/java/com/github/fernandoperc/algorithmscourse/algorithms/unionfind/WeightedQuickUnion.java
@@ -0,0 +1,9 @@
+package com.github.fernandoperc.algorithmscourse.algorithms.unionfind;
+
+public class WeightedQuickUnion extends QuickUnion {
+    
+    public void union(int a, int b) {
+      
+    }
+    
+}
